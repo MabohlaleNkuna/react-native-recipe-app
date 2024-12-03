3c17cb03a02fee3d4d4d954f7477e89854234322
MabohlaleNkuna <mabohlalenkuna@gmail.com>
2024-12-03 04:45:46 +0200

    fixing login and register

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Register from './pages/register'; 
-import Login from './pages/login';
+import Register from './pages/Register'; 
+import Login from './pages/Login';
 import { StatusBar } from 'react-native';
 
 const Stack = createStackNavigator();
